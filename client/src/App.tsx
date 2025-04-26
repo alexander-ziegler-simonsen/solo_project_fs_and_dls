79b93714ea3925677d94e20e16aed8c44c1fec56
Alexander Ziegler <29373163+alexander-ziegler-simonsen@users.noreply.github.com>
2025-04-26 21:31:08 +0200

    initialization of the client

---
+++ client/src/App.tsx
@@ -0,0 +1,15 @@
+import orsLogo from './assets/ors-192x192.png'
+import './App.css'
+
+function App() {
+  return (
+    <>
+      <div>
+        <img src={orsLogo} className="logo" alt="Online Rizz Shop logo" />
+      </div>
+      <h1>Online Rizz Shop</h1>
+    </>
+  )
+}
+
+export default App
