b684b030ba01112c0c071b87eb9ffaafb1f3047e
Alexander Ziegler <29373163+alexander-ziegler-simonsen@users.noreply.github.com>
2025-04-28 22:29:44 +0200

    added a Store file and load it with the main.tsx file

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -2,10 +2,14 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { RouterProvider } from "react-router"
 import Router from './Router.tsx'
+import { Provider } from 'react-redux'
+import Store from './Store.ts'
 
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <RouterProvider router={Router} />
+    <Provider store={Store}>
+      <RouterProvider router={Router} />
+    </Provider>
   </StrictMode>,
 )
