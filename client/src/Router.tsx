6c4ecfb5b1be57de7043278a00d522ab9b6fdf0b
Alexander Ziegler <29373163+alexander-ziegler-simonsen@users.noreply.github.com>
2025-04-28 10:33:57 +0200

    added the paths to Router and it is now catching missing page errors

--- client/src/Router.tsx
+++ client/src/Router.tsx
@@ -2,6 +2,9 @@ import React from 'react'
 import { createBrowserRouter, createRoutesFromElements, Route, Router } from 'react-router'
 import Layout from './Layout'
 import AboutPage from './pages/AboutPage';
+import CategoryPage from './pages/CategoryPage';
+import ProductPage from './pages/ProductPage';
+import Error404Page from './pages/Error404Page';
 
 
 
@@ -9,6 +12,10 @@ import AboutPage from './pages/AboutPage';
     createRoutesFromElements(
       <Route path='/' element={<Layout />}>
         <Route path='/about' element={<AboutPage />} />
+        <Route path='/product' element={<ProductPage />} />
+        <Route path='/category' element={<CategoryPage />} />
+        <Route path='*' element={<Error404Page />} />
+
       </Route>
     )
   );
---
+++ client/src/pages/Error404Page.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function Error404Page() {
+  return (
+    <div>Error 404 - Page was not found</div>
+  )
+}
+
+export default Error404Page
