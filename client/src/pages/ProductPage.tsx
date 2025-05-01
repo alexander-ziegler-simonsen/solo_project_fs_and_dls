f0740afec96722a3fb231131c1e9730cf8385b05
Alexander Ziegler <29373163+alexander-ziegler-simonsen@users.noreply.github.com>
2025-05-01 14:03:27 +0200

    ProductPage takes values from props and handle data in a Product format

--- client/src/pages/ProductPage.tsx
+++ client/src/pages/ProductPage.tsx
@@ -1,11 +1,48 @@
+import { Box, Button, Grid, GridItem, Image, Text } from '@chakra-ui/react'
 import React from 'react'
+import orsLogo from '../assets/ors-192x192.png'
+import { Product } from '../domain/Product';
 
-function ProductPage() {
+function ProductPage(product: Product) {
+
+  // TODO - change layout based on pc or phone
+
+  // <Grid
+  //   templateAreas={{
+  //     base: "main",
+  //     lg: `"aside main"`,
+  //   }}
+  //   templateColumns={{ base: "1fr", lg: "200px 1fr" }}gap="6"></Grid>
+
+  // TODO - remove this data, when we read all data from props 
+
+  const x : Product = {
+    id: 1,
+    name: "banana",
+    price: 1234,
+    info: "we got info here",
+    "description": "we even got description here",
+    image: orsLogo
+  }
+
+  product = x;
+
   return (
-    <div>
-        <h1>ProductPage</h1>
-        <p>This page is one single product. it is the sub page that you get to, each time you click a item.</p>
-    </div>
+    <Grid templateColumns="repeat(4, 1fr)" gap="6">
+      
+      <GridItem colSpan={2} backgroundColor={"blue.100"}>
+        <Image src={product.image} backgroundColor={"blue.100"} />
+      </GridItem>
+      <GridItem colSpan={2} backgroundColor={"blue.100"}>
+        <Text>{product.name}</Text>
+        <Text>{product.price}</Text>
+        <Button>add to cart</Button> 
+        <Text>{product.info}</Text>
+      </GridItem>
+      <GridItem colSpan={4} backgroundColor={"blue.100"}> 
+        <Text>{product.description}</Text>
+      </GridItem>
+    </Grid>
   )
 }
 
