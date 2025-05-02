9a6b9ba74518419a97dea1f02bb0d19f2cf5d0cb
Alexander Ziegler <29373163+alexander-ziegler-simonsen@users.noreply.github.com>
2025-05-02 23:36:56 +0200

    implemented a way for the seeder to create values in the mongo database

--- seeder/seeder.ts
+++ seeder/seeder.ts
@@ -1,13 +1,38 @@
-//import dotenv from "dotenv"
 import mongoose from "mongoose"
 import 'dotenv/config'
 
 const mongodbUrl = process.env.DATABASE_URL
 
-mongoose
-    .connect(mongodbUrl)
+
+
+// the schema that is build like our data 
+// (without id, since mongodb generates those on its own)
+const productSchema = new mongoose.Schema({
+    name: String,
+    price: Number,
+    info: String,
+    description: String,
+    image: String
+})
+
+// mongoose compiles a model, based on our schema
+// it gets the name "product", which later will be the name of the collection 
+// (mongodb adds "s" to the end of the name)
+const product = mongoose.model("product", productSchema);
+
+// some test data
+const productTest = new product({
+    name: "Wireless Mouse",
+    price: 25.99,
+    info: "2.4GHz wireless connectivity",
+    description: "A sleek, ergonomic mouse with adjustable DPI settings and long battery life.",
+    image: "https://example.com/images/mouse1.jpg"
+})
+
+
+mongoose.connect(mongodbUrl) // connection to mongodb
     .then(() => {
-        console.log("database is connected")
+        productTest.save() // save the product to the db
     })
     .catch((err) => {
         console.log(err);
