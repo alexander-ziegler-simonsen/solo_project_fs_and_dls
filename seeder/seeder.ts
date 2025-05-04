d184b0da75bd514f8425a3bd2bfc890f22f6b403
Alexander Ziegler <29373163+alexander-ziegler-simonsen@users.noreply.github.com>
2025-05-04 22:54:34 +0200

    implemented a way for the seeder, to add all Products in the json file

--- seeder/seeder.ts
+++ seeder/seeder.ts
@@ -1,9 +1,12 @@
 import mongoose from "mongoose"
 import 'dotenv/config'
+import * as fs from 'fs'
 
 const mongodbUrl = process.env.DATABASE_URL
 
-
+// some test data
+const rawProducts = fs.readFileSync("./data/Products.json", "utf-8")
+const ProductData = JSON.parse(rawProducts)
 
 // the schema that is build like our data 
 // (without id, since mongodb generates those on its own)
@@ -16,24 +19,41 @@ const productSchema = new mongoose.Schema({
 })
 
 // mongoose compiles a model, based on our schema
-// it gets the name "product", which later will be the name of the collection 
-// (mongodb adds "s" to the end of the name)
-const product = mongoose.model("product", productSchema);
-
-// some test data
-const productTest = new product({
-    name: "Wireless Mouse",
-    price: 25.99,
-    info: "2.4GHz wireless connectivity",
-    description: "A sleek, ergonomic mouse with adjustable DPI settings and long battery life.",
-    image: "https://example.com/images/mouse1.jpg"
-})
+//                              "product", schema, collection name
+const product = mongoose.model("product", productSchema, "products");
 
 
+// TODO - rewrite this code, so it looks better
+
+// delete every product first
 mongoose.connect(mongodbUrl) // connection to mongodb
+.then(() => {
+    // delete every product first
+    product.deleteMany({}) // if there is a empty {} in delete many, it will delete the whole table
     .then(() => {
-        productTest.save() // save the product to the db
+        console.log("all existing products was deleted")
+
+        mongoose.connect(mongodbUrl) // connection to mongodb
+        .then(() => {
+            // save all the products to the db
+            product.insertMany(ProductData).then(() => {
+                console.log("all products added")
+            }).catch((err) => {
+                console.log(err)
+            })
+
+        })
+        .catch((err) => {
+            console.log(err);
+        })
+    })
+.catch((err) => {
+    console.log(err)
+})
+        
     })
     .catch((err) => {
         console.log(err);
     })
+
+
