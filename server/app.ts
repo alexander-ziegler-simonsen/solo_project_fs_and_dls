4d37b06cc807af29a9cfa40c9f7a652ed05a189b
Alexander Ziegler <29373163+alexander-ziegler-simonsen@users.noreply.github.com>
2025-05-21 17:49:09 +0200

    added the base endpoints needed

--- server/app.ts
+++ server/app.ts
@@ -2,11 +2,84 @@ import express from "express";
 
 const app = express();
 
+// product -----------------------------------
 
-app.get("/items", (req, res) => {
-    res.send({ data: "this is the data from express" });
-});
+app.get("/item", (req, res) => {
+    res.send({ data: "this is data" })
+})
 
+app.post("/item", (req, res) => {
+    res.send({ data: "this is data" })
+})
+
+app.put("/item", (req, res) => {
+    res.send({ data: "this is data" })
+})
+
+app.delete("/item", (req, res) => {
+    res.send({ data: "this is data" })
+})
+
+// product_group  -----------------------------------
+
+app.get("/item_group", (req, res) => {
+    res.send({ data: "this is data" })
+})
+
+app.post("/item_group", (req, res) => {
+    res.send({ data: "this is data" })
+})
+
+app.put("/item_group", (req, res) => {
+    res.send({ data: "this is data" })
+})
+
+app.delete("/item_group", (req, res) => {
+    res.send({ data: "this is data" })
+})
+
+// storage  -----------------------------------
+
+
+app.get("/storage", (req, res) => {
+    res.send({ data: "this is data" })
+})
+
+app.post("/storage", (req, res) => {
+    res.send({ data: "this is data" })
+})
+
+app.put("/storage", (req, res) => {
+    res.send({ data: "this is data" })
+})
+
+app.delete("/storage", (req, res) => {
+    res.send({ data: "this is data" })
+})
+
+// user (+ login)  -----------------------------------
+
+app.post("/login", (req, res) => {
+    res.send({ data: "this is data" })
+})
+
+app.get("/user", (req, res) => {
+    res.send({ data: "this is data" })
+})
+
+app.post("/user", (req, res) => {
+    res.send({ data: "this is data" })
+})
+
+app.put("/user", (req, res) => {
+    res.send({ data: "this is data" })
+})
+
+app.delete("/user", (req, res) => {
+    res.send({ data: "this is data" })
+})
+
+// the last part of the server -----------------------------
 
 const PORT = Number(process.env.PORT) || 8080; 
 app.listen(PORT, () => console.log("server is running on port", PORT)) ;
