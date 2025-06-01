85be3658f9eefc925bdd7ac07ca9cd040f751d9d
Alexander Ziegler <29373163+alexander-ziegler-simonsen@users.noreply.github.com>
2025-06-01 23:00:01 +0200

    seeder - call the postgres db creation function

--- seeder/app.ts
+++ seeder/app.ts
@@ -1,8 +1,10 @@
 
 import { setDbAndFirstUser } from "./MongodbSeeder";
+import { setDb } from "./PostgresSeeder";
 
 async function main() {
     await setDbAndFirstUser();
+    await setDb();
 }
 
 main();
