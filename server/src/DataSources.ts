abe13ab8fb3f8d5170653c67740ed195beaa2c84
Alexander Ziegler <29373163+alexander-ziegler-simonsen@users.noreply.github.com>
2025-06-01 22:58:26 +0200

    server - replaced most of the env imports, fixed some host bugs

--- server/src/DataSources.ts
+++ server/src/DataSources.ts
@@ -3,7 +3,6 @@ import 'dotenv/config';
 import "reflect-metadata";
 
 // entites
-//import ItemGroup from "./entities/ItemGroup";
 import { Item, Item_post } from "./entities/Item";
 import { ItemGroup, ItemGroup_post } from "./entities/ItemGroup";
 import { Order, Order_post } from "./entities/Order";
@@ -12,21 +11,24 @@ import { User, User_post } from "./entities/User";
 import {Storage, Storage_post } from "./entities/Storage";
 
 // TODO - fix mongodb port
-const mon_port = process.env.MONGO_PORT;
-const mon_use: string = process.env.MONGO_USERNAME || "username";
-const mon_pass: string = process.env.MONGO_PASSWORD || "password";
-const mon_db = process.env.MONGO_DB;
+const mon_port = process.env.MONGO_PORT || "27018";
+const mon_use: string = process.env.MONGO_NEW_USERNAME || "user1";
+const mon_pass: string = process.env.MONGO_NEW_PASSWORD || "password1";
+const mon_host: string = process.env.MONGO_HOST || "localhost";
+const mon_db = process.env.MONGO_NEW_DB || "rizz";
 
 // TODO - fix postgres port
-const post_port = process.env.POSTGRES_PORT;
-const post_use: string = process.env.POSTGRES_USERNAME || "username";
+const post_port = process.env.POSTGRES_PORT || "5433";
+const post_host = process.env.POSTGRES_HOST || "localhost";
+const post_use: string = process.env.POSTGRES_USERNAME || "admin";
 const post_pass: string = process.env.POSTGRES_PASSWORD || "password";
-const post_db = process.env.POSTGRES_DB;
+const post_db = process.env.POSTGRES_DB || "rizz";
 
 
-export const MongodbDataSource = new DataSource({
+export const MongodbDataSource = new 
+DataSource({
     type: "mongodb",
-    host: "localhost",
+    host: mon_host,
     port: 27018,
     synchronize: true,
     entities: [Item, User, OrderItem, Order, Storage],
@@ -37,7 +39,7 @@ export const MongodbDataSource = new DataSource({
 
 export const PostgresDataSource = new DataSource({
     type: "postgres",
-    host: "localhost",
+    host: post_host,
     port: 5433,
     synchronize: true,
     entities: [Item_post, User_post, OrderItem_post, Order_post, Storage_post],
