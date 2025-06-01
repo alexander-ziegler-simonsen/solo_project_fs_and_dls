c6ac0cdd6960ea2621ea28b10127d33aa39dcd96
Alexander Ziegler <29373163+alexander-ziegler-simonsen@users.noreply.github.com>
2025-06-02 00:17:36 +0200

    QH - remove msg from rabbitmq after use, added a error print if no connection

--- MicroServices/QueueHandler/src/app.ts
+++ MicroServices/QueueHandler/src/app.ts
@@ -5,14 +5,17 @@ import { PostgresDataSource } from "./DataSource";
 import { ManageQueue } from "./Helper/ManageQueues";
 
 const RABBIT_HOST = process.env.RABBITMQ_HOST || "localhost";
-const RABBIT_PORT = process.env.RABBITMQ_PORT || 5672;
-const RABBIT_USERNAME = process.env.RABBITMQ_USERNAME || "username";
+const RABBIT_PORT = process.env.RABBITMQ_PORT || 5673;
+const RABBIT_USERNAME = process.env.RABBITMQ_USERNAME || "admin";
 const RABBIT_PASSWORD = process.env.RABBITMQ_PASSWORD || "password";
 
 async function main() {
 
     try {
         const connection = await amqp.connect(`amqp://${RABBIT_USERNAME}:${RABBIT_PASSWORD}@${RABBIT_HOST}:${RABBIT_PORT}`);
+        connection.on('error', (err) => {
+            console.log("error connecting", err);
+        });
 
         const channel = await connection.createChannel();
 
@@ -31,6 +34,7 @@ async function main() {
 
                     // send to one of our switch handler functions
                     await ManageQueue(messageContent.typeOfAction, messageContent.endpointName, messageContent.data);
+                    channel.ack(msg);
                 }
             });
         }
