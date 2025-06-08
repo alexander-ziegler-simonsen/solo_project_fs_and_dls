ced8a365b1a3c92121683231cc57b7247e4e1f20
Alexander Ziegler <29373163+alexander-ziegler-simonsen@users.noreply.github.com>
2025-06-09 00:49:58 +0200

    seeder - using addData to seed my mongodb instance

--- seeder/app.ts
+++ seeder/app.ts
@@ -1,10 +1,26 @@
 
-import { setDbAndFirstUser } from "./MongodbSeeder";
+import { setDbAndFirstUser, AddData } from "./MongodbSeeder";
 import { setDb } from "./PostgresSeeder";
+
+import { Item } from "./entities/Item";
+import { Order } from "./entities/Order";
+import { OrderItem } from "./entities/OrderItem";
+import { ItemGroup } from "./entities/ItemGroup";
+
+import ItemData from "./data/Items.json";
+import ItemGroupData from "./data/ItemGroup.json";
+import OrderData from "./data/Orders.json";
+import OrderItemData from "./data/OrderItems.json";
+
 
 async function main() {
     await setDbAndFirstUser();
     await setDb();
+
+    await AddData<Item>(ItemData, "items");
+    await AddData<Order>(OrderData, "orders");
+    await AddData<OrderItem>(OrderItemData, "orderItems");
+    await AddData<ItemGroup>(ItemGroupData, "itemGroups");
 }
 
 main();
