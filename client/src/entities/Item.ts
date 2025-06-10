f78171a1a02f1918ec2353826041467a599faa1d
Alexander Ziegler <29373163+alexander-ziegler-simonsen@users.noreply.github.com>
2025-06-10 23:38:25 +0200

    client - item

---
+++ client/src/entities/Item.ts
@@ -0,0 +1,40 @@
+import { Entity, ObjectId, PrimaryGeneratedColumn, ObjectIdColumn, PrimaryColumn, Column } from "typeorm";
+import "reflect-metadata";
+
+@Entity()
+ export class Item{
+
+    @PrimaryColumn()
+    _id: number;
+
+    @Column()
+    name: string;
+
+    @Column()
+    description: string;
+
+    @Column()
+    price: number;
+
+    @Column()
+    fk_group_id: number;
+}
+
+@Entity("Item")
+ export class Item_post{
+
+    @PrimaryGeneratedColumn()
+    _id: number;
+
+    @Column()
+    name: string;
+
+    @Column()
+    description: string;
+
+    @Column("decimal")
+    price: number;
+
+    @Column()
+    fk_group_id: number;
+}
