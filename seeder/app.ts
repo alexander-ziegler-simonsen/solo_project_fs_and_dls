7c32d9e027f5dbca0313b89d4aaabec415ed04b2
Alexander Ziegler <29373163+alexander-ziegler-simonsen@users.noreply.github.com>
2025-06-10 16:11:02 +0200

    seeder - load data into the database

--- seeder/app.ts
+++ seeder/app.ts
@@ -1,26 +1,69 @@
 
 import { setDbAndFirstUser, AddData } from "./MongodbSeeder";
-import { setDb } from "./PostgresSeeder";
+import { setPostgresDb, AddPostgresData, setAllTables, DropTable } from "./PostgresSeeder";
 
 import { Item } from "./entities/Item";
 import { Order } from "./entities/Order";
 import { OrderItem } from "./entities/OrderItem";
 import { ItemGroup } from "./entities/ItemGroup";
+import { User } from "./entities/User";
 
 import ItemData from "./data/Items.json";
 import ItemGroupData from "./data/ItemGroup.json";
 import OrderData from "./data/Orders.json";
 import OrderItemData from "./data/OrderItems.json";
+import UserData from "./data/Users.json";
 
+const itemDescription = {
+    name: "item",
+    keys: ["name","price","info","description","image","fk_group_id"]
+};
+const ItemGroupDescription = {
+    name: "itemGroup",
+    keys: ["name"]
+};
+const OrderDescription = {
+    name: `"order"`,
+    keys: ["fk_user_id"]
+};
+const OrderItemDescription = {
+    name: "orderItem",
+    keys: ["fk_item_id","fk_order_id","count","price"]
+};
+const userDescription = {
+    name: `"user"`,
+    keys: ["username","password","email","phone","address"]
+};
 
 async function main() {
-    await setDbAndFirstUser();
-    await setDb();
+    // mongo
+    //await setDbAndFirstUser();
+    //await AddData<Item>(ItemData, "item");
+    //await AddData<Order>(OrderData, "order");
+    //await AddData<OrderItem>(OrderItemData, "orderItem");
+    //await AddData<ItemGroup>(ItemGroupData, "itemGroup");
 
-    await AddData<Item>(ItemData, "items");
-    await AddData<Order>(OrderData, "orders");
-    await AddData<OrderItem>(OrderItemData, "orderItems");
-    await AddData<ItemGroup>(ItemGroupData, "itemGroups");
+    // postgres
+    await setPostgresDb();
+
+    // drop tables
+    // await DropTable("user");
+    // await DropTable("itemgroup");
+    // await DropTable("item");
+    // await DropTable("order");
+    // await DropTable("orderitem");
+
+    await setAllTables();
+
+    // no fk
+    await AddPostgresData<ItemGroup>(ItemGroupDescription.name, ItemGroupDescription.keys, ItemGroupData);
+    await AddPostgresData<User>(userDescription.name, userDescription.keys, UserData)
+
+    // have fk
+    await AddPostgresData<Item>(itemDescription.name, itemDescription.keys, ItemData)
+    await AddPostgresData<Order>(OrderDescription.name, OrderDescription.keys,  OrderData);
+    await AddPostgresData<OrderItem>(OrderItemDescription.name, OrderItemDescription.keys, OrderItemData);
+    
 }
 
 main();
