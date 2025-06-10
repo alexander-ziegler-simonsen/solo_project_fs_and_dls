09a96129cbf9e3451205c7fb959b7694fc4597f2
Alexander Ziegler <29373163+alexander-ziegler-simonsen@users.noreply.github.com>
2025-06-10 16:08:39 +0200

    server - added 'Access-Control' headers to all my routes

--- server/src/Routes/OrderRouter.ts
+++ server/src/Routes/OrderRouter.ts
@@ -5,17 +5,23 @@ import { PostgresDataSource, MongodbDataSource } from "../DataSources";
 
 import RabbitMQHelper from "../Helpers/RabbitMqHelper";
 
+const CLIENT_HOST = process.env.CLIENT_HOST || "http://localhost:5173";
+
 const OrderRouter = Router();
 const orderRespository = MongodbDataSource.getMongoRepository(Order);
 
 // mongo
 OrderRouter.get("/order", async (req, res) => {
+    res.header("Access-Control-Allow-Origin", CLIENT_HOST);
+    res.header("Access-Control-Allow-Headers", "origin, X-Requested-With, Content-Type");
     let output = await orderRespository.find();
 
     res.send({ data: output });
 })
 
 OrderRouter.get("/order/:id", async (req, res) => {
+    res.header("Access-Control-Allow-Origin", CLIENT_HOST);
+    res.header("Access-Control-Allow-Headers", "origin, X-Requested-With, Content-Type");
     let orderId = req.params.id;
     let output = await orderRespository.findOneBy({id: orderId});
 
@@ -24,19 +30,25 @@ OrderRouter.get("/order/:id", async (req, res) => {
 
 // postgres
 OrderRouter.post("/order", async (req, res) => {
-const rabbitHelper = new RabbitMQHelper();
+    const rabbitHelper = new RabbitMQHelper();
+    res.header("Access-Control-Allow-Origin", CLIENT_HOST);
+    res.header("Access-Control-Allow-Headers", "origin, X-Requested-With, Content-Type");
     let response = rabbitHelper.handlePostToChannel("posts", "post", "order", req.body );
     response ? res.status(200).send("data queued for creation") : res.status(500).send("something went wrong");
 })
 
 OrderRouter.put("/order", async (req, res) => {
 const rabbitHelper = new RabbitMQHelper();
+    res.header("Access-Control-Allow-Origin", CLIENT_HOST);
+    res.header("Access-Control-Allow-Headers", "origin, X-Requested-With, Content-Type");
     let response = rabbitHelper.handlePostToChannel("updates", "put", "order", req.body );
     response ? res.status(200).send("data queued for update") : res.status(500).send("something went wrong");
 })
 
 OrderRouter.delete("/order", async (req, res) => {
 const rabbitHelper = new RabbitMQHelper();
+    res.header("Access-Control-Allow-Origin", CLIENT_HOST);
+    res.header("Access-Control-Allow-Headers", "origin, X-Requested-With, Content-Type");
     let response = rabbitHelper.handlePostToChannel("deletes", "delete", "order", req.body );
     response ? res.status(200).send("data queued for deletion") : res.status(500).send("something went wrong");
 })
