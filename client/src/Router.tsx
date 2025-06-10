63c55673755d2aa21b0c65454e8ec6ced5a95b62
Alexander Ziegler <29373163+alexander-ziegler-simonsen@users.noreply.github.com>
2025-06-10 21:44:25 +0200

    client - added the new pages to the router controller

--- client/src/Router.tsx
+++ client/src/Router.tsx
@@ -4,14 +4,17 @@ import AboutPage from './pages/AboutPage';
 import CategoryPage from './pages/CategoryPage';
 import ProductPage from './pages/ProductPage';
 import Error404Page from './pages/Error404Page';
-
+import LoginPage from './pages/LoginPage';
+import NewAccountPage from "./pages/newAccountPage";
 
   const router = createBrowserRouter(
     createRoutesFromElements(
       <Route path='/' element={<Layout />}>
         <Route path='/about' element={<AboutPage />} />
-        {<Route path='/product' element={<ProductPage />} />}
+        <Route path='/product' element={<ProductPage />} />
         <Route path='/category' element={<CategoryPage />} />
+        <Route path='/login' element={<LoginPage />} />
+        <Route path='/new_account' element={<NewAccountPage />} />
         <Route path='*' element={<Error404Page />} />
 
       </Route>
