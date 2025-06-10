afa4f85b4c45a2211dd6628ea191ea5180325a36
Alexander Ziegler <29373163+alexander-ziegler-simonsen@users.noreply.github.com>
2025-06-10 16:43:59 +0200

    client - testing the api caller implementation

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,12 +1,25 @@
-import orsLogo from './assets/ors-192x192.png'
+import { Button } from '@chakra-ui/react';
+import orsLogo from './assets/ors-192x192.png';
+import { getData } from './helpers/HandleApiCalls';
 
 function App() {
+
+  const temp = () => { 
+    console.log(getData("item")); 
+  };
+  
   return (
     <>
       <div>
         <img src={orsLogo} className="logo" alt="Online Rizz Shop logo" />
       </div>
       <h1>Online Rizz Shop - app page</h1>
+
+      <p>
+        {/* {JSON.stringify(temp)} */}
+        <Button 
+        onClick={ () => temp() } >test</Button>
+      </p>
     </>
   )
 }
