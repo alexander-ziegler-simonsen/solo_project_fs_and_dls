1048c9018b9325468221f7febe54c03c09139051
Alexander Ziegler <29373163+alexander-ziegler-simonsen@users.noreply.github.com>
2025-06-11 00:48:41 +0200

    small changes

--- client/src/pages/CategoryPage.tsx
+++ client/src/pages/CategoryPage.tsx
@@ -1,6 +1,24 @@
 // import React from 'react'
 
+import { useEffect, useState } from "react"
+import { getData } from "../helpers/HandleApiCalls";
+
+type ApiResponse = {
+  message: string;
+  timestamp: number;
+};
+
 function CategoryPage() {
+
+  const [data, setData] = useState<ApiResponse | null>(null);;
+  const [error, setError] = useState<string | null>(null);
+
+  useEffect(() => {
+    getData("item").then((res) => {
+      //return res.json();
+    })
+  })
+
   return (
     <div>
         <h1>CategoryPage</h1>
