09fe80c162239d938e3d9d2bd8f9daf54862878a
Alexander Ziegler <29373163+alexander-ziegler-simonsen@users.noreply.github.com>
2025-06-15 00:48:58 +0200

    client - first version of the Account info page

---
+++ client/src/pages/account/AccountInfoPage.tsx
@@ -0,0 +1,52 @@
+import { Button, Container, Field, Input, Text } from "@chakra-ui/react"
+import { PasswordInput } from "../../components/ui/password-input"
+import { useState } from "react";
+
+
+function AccountInfoPage() {
+    
+      const [formData, setFormData] = useState({
+        fname: "first name",
+        lname: "last name",
+        email: "mail@mail.com",
+        phone: "12341234",
+        address: "address 1234",
+        username: "username",
+        password: "password",
+      });
+
+    return (
+        <>
+            <Container>
+                <Text>account info</Text>
+                <Text>see your info here and change it</Text>
+
+                <Field.Root>
+                    <Field.Label id="fname">firstname</Field.Label>
+                    <Input bg="white" id="in_fname" placeholder="firstname" variant="outline" value={formData.fname} />
+
+                    <Field.Label id="lname">lastname</Field.Label>
+                    <Input bg="white" id="in_lname" placeholder="lastname" variant="outline" value={formData.lname} />
+
+                    <Field.Label id="email">email</Field.Label>
+                    <Input bg="white" id="in_email" placeholder="email" variant="outline" value={formData.email} />
+
+                    <Field.Label id="phone">phone number</Field.Label>
+                    <Input bg="white" id="in_phone" placeholder="phone number" variant="outline" value={formData.phone} />
+
+                    <Field.Label id="address">address</Field.Label>
+                    <Input bg="white" id="in_address" placeholder="address" variant="outline" value={formData.address} />
+
+                    <Field.Label id="username">Username</Field.Label>
+                    <Input bg="white" id="in_username" placeholder="username" variant="outline" value={formData.username} />
+
+                    <Field.Label id="password">password</Field.Label>
+                    <PasswordInput bg="white" id="in_password" placeholder="password" variant="outline" value={formData.password} />
+                </Field.Root>
+                <Button onClick={() => {console.log("button clicked - account info")} }>save all changes</Button>
+            </Container>
+        </>
+    )
+}
+
+export default AccountInfoPage
