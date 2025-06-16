4a401d09ce1c95013c178fd265dbecbb6cca0461
Alexander Ziegler <29373163+alexander-ziegler-simonsen@users.noreply.github.com>
2025-06-16 22:35:59 +0200

    client - removed redux from these files

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -2,16 +2,14 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { RouterProvider } from "react-router"
 import Router from './Router.tsx'
-import { Provider } from 'react-redux'
+// import { Provider } from 'react-redux'
 import { Store } from './redux/Store.ts'
 import { Provider as ChrakaProvider } from './components/ui/provider'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <Provider store={Store}>
       <ChrakaProvider>
         <RouterProvider router={Router} />
       </ChrakaProvider>
-    </Provider>
   </StrictMode>,
 )
--- client/src/pages/AboutPage.tsx
+++ client/src/pages/AboutPage.tsx
@@ -1,10 +1,10 @@
 import { Button, Flex, Text } from '@chakra-ui/react'
 // import React from 'react'
-import { useAppDispatch, useAppSelector } from "../hooks/ReduxHook"
+//import { useAppDispatch, useAppSelector } from "../hooks/ReduxHook"
 import { increment, decrement } from '../redux/counter/counterSlice'
 function AboutPage() {
-  const count = useAppSelector((state) => state.counter.value)
-  const dispatch = useAppDispatch()
+  // const count = useAppSelector((state) => state.counter.value)
+  // const dispatch = useAppDispatch()
 
   return (
     <div>
@@ -12,9 +12,9 @@ function AboutPage() {
       <p>here we will write all the text, that talks about how good our website is.</p>
       <div>
             <Flex gap={5}>
-                <Button onClick={ () => dispatch(increment()) } >add 1</Button>
+                {/* <Button onClick={ () => dispatch(increment()) } >add 1</Button>
                 <Button onClick={ () => dispatch(decrement()) }>remove 1</Button>
-                <Text>current value : { count }</Text>
+                <Text>current value : { count }</Text> */}
             </Flex>
         </div>
     </div>
