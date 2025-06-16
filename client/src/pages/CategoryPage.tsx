3b4506f660f5ffb0b178901e38399d35da657f27
Alexander Ziegler <29373163+alexander-ziegler-simonsen@users.noreply.github.com>
2025-06-16 18:10:34 +0200

    client - implemented products on categoryPage

--- client/src/pages/CategoryPage.tsx
+++ client/src/pages/CategoryPage.tsx
@@ -2,28 +2,46 @@
 
 import { useEffect, useState } from "react"
 import { getData } from "../helpers/HandleApiCalls";
+import ProductElement from "../components/ProductElement";
+import { Item } from "../domain/Item";
+import { Container, Grid, GridItem, Text } from "@chakra-ui/react";
 
-type ApiResponse = {
-  message: string;
-  timestamp: number;
-};
-
+// TODO - check if our page being async, gives problem in the rest of the app
 function CategoryPage() {
 
-  const [data, setData] = useState<ApiResponse | null>(null);;
-  const [error, setError] = useState<string | null>(null);
+  const [items, setItems] = useState<Item[]>([]);
+  const [loading, setLoading] = useState(true);
 
   useEffect(() => {
-    getData("item").then((res) => {
-      //return res.json();
-    })
-  })
+    async function fetchItems() {
+      try {
+        const data: Item[] = await getData<Item>("item");
+        setItems(data);
+      } catch (err) {
+        console.error("Failed to fetch items", err);
+      } finally {
+        setLoading(false);
+      }
+    }
+
+    fetchItems();
+  }, []);
 
   return (
-    <div>
-        <h1>CategoryPage</h1>
-        <p>here will be able to see all the products, sorted or filtered after which category they come from.</p>
-    </div>
+    <Container>
+      <Text>CategoryPage</Text>
+      <Text>Here you will be able to see all the products, sorted or filtered by category.</Text>
+      {loading && <p>Loading...</p>}
+
+      <Grid templateColumns={"repeat(4, 1fr)"} gap={3}>
+        {
+          items.map((item) => (
+            <GridItem colSpan={1}><ProductElement key={item._id} ItemValue={item} /></GridItem>
+          ))
+        }
+      </Grid>
+
+    </Container>
   )
 }
 
