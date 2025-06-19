9615c64d1064e217f85660fe75b49332f09d084c
Alexander Ziegler <29373163+alexander-ziegler-simonsen@users.noreply.github.com>
2025-06-19 10:01:01 +0200

    client - first version of login

--- client/src/pages/LoginPage.tsx
+++ client/src/pages/LoginPage.tsx
@@ -1,8 +1,29 @@
 import { Box, Field, Center, Input, VStack, Button, HStack } from "@chakra-ui/react"
 import { useLocation, useNavigate } from 'react-router';
+import { Login} from "../helpers/HandleApiCalls";
+import { useState } from "react";
 
 
 function LoginPage() {
+
+  const [formData, setFormData] = useState({
+    username: '',
+    password: '',
+  });
+
+  const checkLogin = (username: string, password: string) => {
+    // TODO - do something to the password, before we send it
+    let output = Login({username: username, password: password});
+    
+  }
+
+  const handleChange = (e: any) => {
+    const { name, value } = e.target;
+    setFormData(prev => ({
+      ...prev,
+      [name]: value,
+    }));
+  };
 
   const navigate = useNavigate();
 
@@ -11,16 +32,16 @@ function LoginPage() {
       <Box w={500}>
         <Field.Root>
           <Field.Label>Username</Field.Label>
-          <Input bg="white" placeholder="username" variant="outline" />
+          <Input id="input_username" bg="white" name="username" value={formData.username} onChange={handleChange}
+          placeholder="username" variant="outline" />
           <Field.Label>password</Field.Label>
-          <Input bg="white" placeholder="password" variant="outline" />
+          <Input id="input_password" bg="white" name="password" value={formData.password} onChange={handleChange}
+           placeholder="password" variant="outline" />
         </Field.Root>
         <Center>
           <HStack margin={3}>
-            <Button>Login</Button>
-            <Button onClick={() => {
-              console.log("a button was pressed");
-              navigate("/new_account");  }}>make new account</Button>
+            <Button onClick={() => { checkLogin(formData.username, formData.password); } }>Login</Button>
+            <Button onClick={() => { navigate("/new_account");} }>make new account</Button> 
           </HStack>
         </Center>
       </Box>
