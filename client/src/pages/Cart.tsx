c6ecbd2372b6f1381ea30bd90b4dd445e87834bd
Alexander Ziegler <29373163+alexander-ziegler-simonsen@users.noreply.github.com>
2025-06-24 22:34:08 +0200

    client - better design on the cart page

--- client/src/pages/Cart.tsx
+++ client/src/pages/Cart.tsx
@@ -1,4 +1,4 @@
-import { Container } from "@chakra-ui/react"
+import { Button, Container, Image, Table } from "@chakra-ui/react"
 import { useCartStore } from "../useCartStore";
 
 function Cart() {
@@ -9,12 +9,43 @@ function Cart() {
         <Container>
             <div className="cart">
                 <h2>Cart</h2>
-                {cartItems.map(({ product, amount }) => (
-                    <div key={product._id}>
-                        <span>{product.name} | count | {amount} |</span>
-                        <button onClick={() => removeFromCart(product._id)}>Remove</button>
-                    </div>
-                ))}
+
+                <Table.Root>
+                    <Table.Header>
+                        <Table.Row>
+                            <Table.ColumnHeader>name</Table.ColumnHeader>
+                            <Table.ColumnHeader></Table.ColumnHeader>
+                            <Table.ColumnHeader>count</Table.ColumnHeader>
+                            <Table.ColumnHeader>price pr unit</Table.ColumnHeader>
+                            <Table.ColumnHeader>options</Table.ColumnHeader>
+                        </Table.Row>
+                    </Table.Header>
+                    <Table.Body>
+                        {cartItems.map(({ product, amount }) => (
+                            <Table.Row key={product._id}>
+                                <Table.Cell>{product.name}</Table.Cell>
+                                <Table.Cell><Image maxH={100} maxW={100} src={product.image} /></Table.Cell>
+                                <Table.Cell>{amount}</Table.Cell>
+                                <Table.Cell>{product.price}</Table.Cell>
+                                <Table.Cell>
+                                    <Button onClick={() => removeFromCart(product._id)}>Remove</Button>
+                                </Table.Cell>
+
+                            </Table.Row>
+                        ))}
+                    </Table.Body>
+                    <Table.Footer>
+                        <Table.Row bg={"gray.200"}>
+                            <Table.Cell >in total: </Table.Cell>
+                            <Table.Cell></Table.Cell>
+                            <Table.Cell>x </Table.Cell>
+                            <Table.Cell>xx.xxx kr</Table.Cell>
+                            <Table.Cell></Table.Cell>
+                        </Table.Row>
+                    </Table.Footer>
+                </Table.Root>
+
+
             </div>
         </Container>
     )
