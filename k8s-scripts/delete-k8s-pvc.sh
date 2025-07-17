7a02c234be39b8e74788dcdc5e48fb0cc91a27b7
Alexander Ziegler <29373163+alexander-ziegler-simonsen@users.noreply.github.com>
2025-07-17 23:27:31 +0200

    moved my files out of "rod" folder and did some changes

---
+++ k8s-scripts/delete-k8s-pvc.sh
@@ -0,0 +1,11 @@
+echo "deleting all persistent data from k8s volumes..."
+
+kubectl delete pvc -n ors postgres-svc
+kubectl delete pvc -n ors mongo-svc
+kubectl delete pvc -n ors seeder-svc
+kubectl delete pvc -n ors rabbitmq-svc
+kubectl delete pvc -n ors client-svc
+kubectl delete pvc -n ors api-svc
+kubectl delete pvc -n ors queue-handler-svc
+
+echo "done deleting all persistent data from k8s..."
