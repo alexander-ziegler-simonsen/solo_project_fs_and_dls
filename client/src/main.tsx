8f8dfcb8eb38c2460b9a0e327aea42dfad1e6ec0
Alexander Ziegler <29373163+alexander-ziegler-simonsen@users.noreply.github.com>
2025-08-03 23:44:36 +0200

    client - added tanstack, some cleanup

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -3,11 +3,18 @@ import { createRoot } from 'react-dom/client'
 import { RouterProvider } from "react-router"
 import Router from './Router.tsx'
 import { Provider as ChrakaProvider } from './components/ui/provider'
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
+
+const queryClient = new QueryClient()
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-      <ChrakaProvider>
-        <RouterProvider router={Router} />
-      </ChrakaProvider>
+      <QueryClientProvider client={queryClient}>
+        <ChrakaProvider>
+          <RouterProvider router={Router} />
+        </ChrakaProvider>
+        <ReactQueryDevtools />
+      </QueryClientProvider>
   </StrictMode>,
 )
