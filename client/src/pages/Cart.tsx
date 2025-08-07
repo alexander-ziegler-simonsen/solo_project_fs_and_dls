c86ca80df3a4f6e71e20210d6625488ea734c926
Alexander Ziegler <29373163+alexander-ziegler-simonsen@users.noreply.github.com>
2025-08-07 21:25:02 +0200

    client - added better design on the cart page

--- client/src/pages/Cart.tsx
+++ client/src/pages/Cart.tsx
@@ -1,107 +1,102 @@
-import { Box, Button, Container, Flex, HStack, Image, Separator, Table, Tooltip, VStack } from "@chakra-ui/react"
+import { Box, Button, Center, Container, Flex, HStack, Image, Separator, Stack, Table, Text, Tooltip, VStack } from "@chakra-ui/react"
 import { useCartStore } from "../useCartStore";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faAdd, faRemove, faSubtract } from "@fortawesome/free-solid-svg-icons";
 
 function Cart() {
+
+    const countAllItems = function () {
+        if (cartItems.length > 0) {
+            let output = 0;
+
+            for (const item of cartItems) {
+                output += item.amount;
+            }
+
+            return output;
+        }
+
+        return 0;
+    }
+
+    const countFullPrice = function () {
+        if (cartItems.length > 0) {
+            let output = 0;
+
+            for (const item of cartItems) {
+                output += item.product.price;
+            }
+
+            return output;
+        }
+
+        return 0;
+    }
+
     const cartItems = useCartStore((state) => state.items);
     const removeFromCart = useCartStore((state) => state.removeFromCart);
 
     return (
-        <Container>
+        <>
+            <h2>Cart</h2>
+            <Container>
 
-            <div className="cart">
-                <h2>Cart</h2>
+                <Stack bg={"bg"} direction={{ base: "column", sm: "column", md: "row" }}>
 
-                <HStack>
-                    <Flex direction={{ sm: "column", md: "row" }} gap={4}> 
-                        <Box flex={{base: "full", md: "10vw"}} bg={"purple.100"} p={4}>
-                            {cartItems.map(({ product, amount }) => (
+                    <Box p={3} w={{ base: "full", sm: "full", md: "80.8%", }} textAlign={'center'} bg={'bg'}>
+                        <Table.Root size={{ base: "sm", md: "md", lg: "lg" }}>
 
-                                <HStack backgroundColor={"blue.200"} gap={"5rem"} border={"solid 1px black"} p={"5px"} m={"2px"}>
-                                    {/* button */}
-                                    <Button onClick={() => removeFromCart(product._id)}>
-                                        <FontAwesomeIcon icon={faRemove} size="xl" />
-                                    </Button>
-
-                                    {/* image */}
-                                    <Image maxH={100} maxW={100} src={product.image} />
-
-                                    {/* info */}
-                                    <VStack>
-                                        <div>{product.name}</div>
-                                        <div>id: {product._id}</div>
-                                    </VStack>
-
-                                    {/* options */}
-                                    <HStack>
-                                        <Button>
-                                            <FontAwesomeIcon icon={faSubtract} />
-                                        </Button>
-                                        <Box>{amount}x</Box>
-                                        <Button>
-                                            <FontAwesomeIcon icon={faAdd} />
-                                        </Button>
-                                    </HStack>
-
-                                    {/* price */}
-                                    <VStack>
-                                        <div>price pr unit: {product.price}</div>
-                                        <div>full price: {Math.round(product.price * amount)}</div>
-                                    </VStack>
-
-                                </HStack>
-
-                            ))}
-                        </Box>
-                        <Box flex={{ base: "1 0 100%" , md: "30vw"}} bg={"orange.100"} p={4} marginEnd={"auto"}>
-                            <VStack backgroundColor={"blue.200"} border={"solid 1px black"} p={"5px"} m={"2px"}>
-                                <div>amount of items: x</div>
-
-                                <div>price in total: x</div>
-                            </VStack>
-                        </Box>
-                    </Flex>
-                </HStack>
-
-                {/* <Table.Root>
-                    <Table.Header>
-                        <Table.Row>
-                            <Table.ColumnHeader>name</Table.ColumnHeader>
-                            <Table.ColumnHeader></Table.ColumnHeader>
-                            <Table.ColumnHeader>count</Table.ColumnHeader>
-                            <Table.ColumnHeader>price pr unit</Table.ColumnHeader>
-                            <Table.ColumnHeader>options</Table.ColumnHeader>
-                        </Table.Row>
-                    </Table.Header>
-                    <Table.Body>
-                        {cartItems.map(({ product, amount }) => (
-                            <Table.Row key={product._id}>
-                                <Table.Cell>{product.name}</Table.Cell>
-                                <Table.Cell><Image maxH={100} maxW={100} src={product.image} /></Table.Cell>
-                                <Table.Cell>{amount}</Table.Cell>
-                                <Table.Cell>{product.price}</Table.Cell>
-                                <Table.Cell>
-                                    <Button onClick={() => removeFromCart(product._id)}>Remove</Button>
-                                </Table.Cell>
-
-                            </Table.Row>
-                        ))}
-                    </Table.Body>
-                    <Table.Footer>
-                        <Table.Row bg={"gray.200"}>
-                            <Table.Cell >in total: </Table.Cell>
-                            <Table.Cell></Table.Cell>
-                            <Table.Cell>x </Table.Cell>
-                            <Table.Cell>xx.xxx kr</Table.Cell>
-                            <Table.Cell></Table.Cell>
-                        </Table.Row>
-                    </Table.Footer>
-                </Table.Root> */}
+                            <Table.Body >
+                                {cartItems.map(({ product, amount }) => (
+                                    <Table.Row key={product._id} bg={"accent"}>
+                                        <Table.Cell borderColor={"border"} border={"1px solid"}>
+                                            <Button 
+                                            bg={"danger"} 
+                                            size={{ base: "xs", md: "md", lg: "lg" }} 
+                                            variant={"solid"} 
+                                            onClick={() => removeFromCart(product._id)}>X</Button>
+                                        </Table.Cell>
+                                        <Table.Cell bg={"white"} borderColor={"border"} border={"1px solid"}>
+                                            <Center>
+                                                <Image p={2} bg={"white"} maxH={"125px"} src={product.image} />
+                                            </Center>
+                                        </Table.Cell>
+                                        <Table.Cell borderColor={"border"} border={"1px solid"}>{product.name}</Table.Cell>
+                                        <Table.Cell borderColor={"border"} border={"1px solid"}>
+                                            <Stack direction={{ base: "column", sm: "column", md: "row" }}>
+                                                <Button bg={"primary"} size={{ base: "xs", md: "md", lg: "lg" }}> - </Button>
+                                                <Box>{amount}x</Box>
+                                                <Button bg={"primary"} size={{ base: "xs", md: "md", lg: "lg" }}> + </Button>
+                                            </Stack>
+                                        </Table.Cell>
+                                        <Table.Cell borderColor={"border"} border={"1px solid"}>
+                                            <Text>pr unit: {product.price} kr</Text><br />
+                                            {amount > 1 ? <Text>full: {Math.round(product.price * amount)} kr</Text> : ""}
+                                        </Table.Cell>
+                                    </Table.Row>
+                                ))}
+                            </Table.Body>
+                        </Table.Root>
+                    </Box>
 
 
-            </div>
-        </Container>
+                    <Box p={4} w={{ base: "full", sm: "full", md: "20.8%", }} bg={"bg"}>
+                        <VStack 
+                            bg={"accent"} border={"solid 1px"} borderColor={"border"} p={"5px"} m={"2px"}>
+                            <Text>amount of items: {countAllItems()}</Text>
+
+                            <Text>price in total: {countFullPrice()} kr</Text>
+                        </VStack>
+
+                        <Button bg={"primary"} onClick={() => {console.log("make order")} }>order</Button>
+                    </Box>
+
+
+
+
+                </Stack>
+            </Container>
+        </>
     )
 }
 
