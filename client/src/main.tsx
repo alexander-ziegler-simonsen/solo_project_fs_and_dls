158ac4bf8346a7de6c78452034f13b369592947e
Alexander Ziegler <29373163+alexander-ziegler-simonsen@users.noreply.github.com>
2025-08-07 20:34:30 +0200

    client - renamed provider

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -2,7 +2,7 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { RouterProvider } from "react-router"
 import Router from './Router.tsx'
-import { Provider as ChrakaProvider } from './components/ui/provider'
+import { Provider as ChakraUiProviderV3 } from './components/ui/provider'
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
 import { ModalProvider } from './context/ModalContext.tsx'
@@ -13,9 +13,9 @@ createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <QueryClientProvider client={queryClient}>
       <ModalProvider>
-        <ChrakaProvider>
+        <ChakraUiProviderV3>
           <RouterProvider router={Router} />
-        </ChrakaProvider>
+        </ChakraUiProviderV3>
       </ModalProvider>
       <ReactQueryDevtools />
     </QueryClientProvider>
