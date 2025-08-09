0e0327b21c8d9be82f5d397db5b05764c33798af
Alexander Ziegler <29373163+alexander-ziegler-simonsen@users.noreply.github.com>
2025-08-09 16:03:41 +0200

    QH - fixed missing port

--- MicroServices/QueueHandler/src/DataSource.ts
+++ MicroServices/QueueHandler/src/DataSource.ts
@@ -10,8 +10,7 @@ import { OrderItem_post } from "./entities/OrderItem";
 import { User_post } from "./entities/User";
 import { Storage_post } from "./entities/Storage";
 
-// TODO - fix postgres port
-const post_port = process.env.POSTGRES_PORT;
+const post_port: number = parseInt(process.env.POSTGRES_PORT) || 5433;
 const post_use: string = process.env.POSTGRES_USERNAME || "admin";
 const post_pass: string = process.env.POSTGRES_PASSWORD || "password";
 const post_db = process.env.POSTGRES_DB || "rizz";
@@ -20,7 +19,7 @@ const post_host = process.env.POSTGRES_HOST || "localhost";
 export const PostgresDataSource = new DataSource({
     type: "postgres",
     host: post_host,
-    port: 5432,
+    port: post_port,
     synchronize: true,
     entities: [Item_post, User_post, OrderItem_post, Order_post, Storage_post, ItemGroup_post],
     username: post_use,
