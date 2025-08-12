e8f7b6bea5758cddc2626121b50cfd25d76d7688
Alexander Ziegler <29373163+alexander-ziegler-simonsen@users.noreply.github.com>
2025-08-12 15:42:26 +0200

    seeder - moved files, made a typeorm DataSource for mongodb

---
+++ seeder/src/PostgresSeeder.ts
@@ -0,0 +1,360 @@
+import { Client, Pool } from "pg";
+import 'dotenv/config';
+
+const postgres_user = process.env.POSTGRES_USERNAME || "admin";
+const postgres_pass = process.env.POSTGRES_PASSWORD || "password";
+const postgres_host = process.env.POSTGRES_HOST || "localhost";
+const postgres_port = process.env.POSTGRES_PORT || 5433;
+const postgres_db = process.env.POSTGRES_DB || "rizz";
+
+const FIRST_URL = `posgres://${postgres_user}:${postgres_pass}@${postgres_host}:${postgres_port}`; // or your MongoDB connection string
+const URL = `posgres://${postgres_user}:${postgres_pass}@${postgres_host}:${postgres_port}/${postgres_db}`;
+
+
+const client = new Client({ connectionString: URL });
+
+// pool.ON('error', (err, client) => {
+//     console.error('Unexpected error on idle client', err);
+//     process.exit(-1);
+// })
+
+//posgres
+
+export async function setPostgresDb() {
+    const firstClient = await new Client({ connectionString: FIRST_URL });
+
+    try {
+        console.log("posgres-setPostgresDb was called");
+        // TODO - check if this is the first time this code is running, if no, then don't run it
+        await firstClient.connect();
+        const res1 = await firstClient.query(`CREATE DATABASE ${postgres_db};`);
+        //console.log(`CREATE DATABASE ${postgres_db};`, res1.rows[0].message);
+
+        // give the 'user privilege to this new db
+        const res2 = await firstClient.query(`GRANT ALL PRIVILEGES ON DATABASE ${postgres_db} TO ${postgres_user};`);
+        //console.log(`GRANT ALL PRIVILEGES ON DATABASE ${postgres_db} TO ${postgres_user};`, res2.rows[0].message);
+    } catch (err) {
+        console.error("Error creating db:", err);
+    } finally {
+        await firstClient.end();
+    }
+}
+
+export async function setAllTables() {
+    console.log("posgres-setAllTables was called");
+    const pool = new Pool({
+        user: postgres_user,
+        host: postgres_host,
+        database: postgres_db,
+        password: postgres_pass,
+        port: postgres_port
+    })
+
+    const poolClient = await pool.connect();
+
+    const userT = `CREATE TABLE IF NOT EXISTS "user" (
+            _id SERIAL PRIMARY KEY,
+            username TEXT NOT NULL UNIQUE,
+            password TEXT NOT NULL,
+            email TEXT NOT NULL UNIQUE,
+            phone TEXT NOT NULL,
+            address TEXT NOT NULL
+        );`
+
+    const ItemGroupT = `CREATE TABLE IF NOT EXISTS itemGroup (
+            _id SERIAL PRIMARY KEY,
+            name TEXT NOT NULL
+        );`
+
+    const OrderT = `CREATE TABLE IF NOT EXISTS "order" (
+            _id SERIAL PRIMARY KEY,
+            fk_user_id INTEGER NOT NULL REFERENCES "user"(_id)
+        );`
+
+    const ItemT = `CREATE TABLE IF NOT EXISTS item (
+            _id SERIAL PRIMARY KEY,
+            name TEXT NOT NULL,
+            price NUMERIC(10, 2) NOT NULL,
+            info TEXT NOT NULL,
+            description TEXT NOT NULL,
+            image TEXT NOT NULL,
+            fk_group_id INTEGER REFERENCES itemGroup(_id)
+        );`;
+
+    const OrderItemT = `CREATE TABLE IF NOT EXISTS orderItem (
+            _id SERIAL PRIMARY KEY,
+            fk_item_id INTEGER NOT NULL REFERENCES item(_id),
+            fk_order_id INTEGER NOT NULL REFERENCES "order"(_id),
+            count INTEGER NOT NULL,
+            price NUMERIC(10, 2) NOT NULL
+        );`;
+
+    // const allTables = `BEGIN;        
+    // COMMIT;`;
+    try {
+        await poolClient.query("BEGIN");
+        console.log("BEGIN was called")
+
+        await poolClient.query(userT);
+        console.log("poolClient.query(userT) - was called")
+
+        await poolClient.query(ItemGroupT);
+        console.log("poolClient.query(ItemGroupT) - was called")
+
+        await poolClient.query(OrderT);
+        console.log("poolClient.query(OrderT) - was called")
+
+        await poolClient.query(ItemT);
+        console.log("poolClient.query(ItemT) - was called")
+
+        await poolClient.query(OrderItemT);
+        console.log("poolClient.query(OrderItemT) - was called")
+
+
+        await poolClient.query("COMMIT");
+        console.log("poolClient.query(COMMIT) - was called")
+
+    } catch (err) {
+        console.error("posgres-Error creating db:", err);
+    } finally {
+        await poolClient.release();
+        await poolClient.end();
+    }
+}
+
+export async function DropTable(tableToDrop) {
+    console.log("posgres-DropTable was called");
+    const pool = new Pool({
+        user: postgres_user,
+        host: postgres_host,
+        database: postgres_db,
+        password: postgres_pass,
+        port: postgres_port
+    })
+
+    const poolClient = await pool.connect();
+    try {
+
+
+
+        await poolClient.query(`DROP TABLE ${tableToDrop}`);
+
+
+        // await poolClient.query("COMMIT");
+    } catch (err) {
+        console.error("posgres-Error adding Items:", err);
+    } finally {
+        await poolClient.release();
+        await poolClient.end();
+    }
+}
+
+export async function AddPostgresData<T>(tableName: string, keys: string[], insertData: T[]) {
+    console.log("posgres-AddPostgresData was called");
+    //await client.connect();
+    const pool = new Pool({
+        user: postgres_user,
+        host: postgres_host,
+        database: postgres_db,
+        password: postgres_pass,
+        port: postgres_port
+    })
+
+    const poolClient = await pool.connect();
+
+
+    try {
+
+        // empty the whole table first
+        //await poolClient.query(`DELETE FROM ${tableName}`);
+        //await poolClient.query("COMMIT");
+
+        const values: string[] = [];
+        const params: any[] = [];
+
+        insertData.forEach((row, rowIndex) => {
+            const paramPlaceholders = keys.map((_, colIndex) => {
+                return `$${rowIndex * keys.length + colIndex + 1}`;
+            });
+
+            values.push(`(${paramPlaceholders.join(', ')})`);
+
+            // Push the values in the correct column order
+            keys.forEach((col) => {
+                params.push((row as any)[col]);
+            });
+        })
+
+        const query = { text: `INSERT INTO ${tableName} (${keys.join(', ')}) VALUES ${values.join(', ')} RETURNING *` };
+
+        let res = await poolClient.query(query, params);
+
+        // await poolClient.query("COMMIT");
+    } catch (err) {
+        console.error("posgres-Error adding Items:", err);
+    } finally {
+        await poolClient.release();
+        await poolClient.end();
+    }
+}
+
+// we get all the merge data, which we need for the item documents on mongodb
+export async function GetPostItems() {
+
+    console.log("GetPostItems was called");
+
+    const pool = new Pool({
+        user: postgres_user,
+        host: postgres_host,
+        database: postgres_db,
+        password: postgres_pass,
+        port: postgres_port
+    })
+
+    //console.log("pool", pool);
+
+
+
+    const poolClient = await pool.connect();
+
+    //console.log("pool", poolClient);
+
+    let output;
+
+    try {
+        const query = {
+            text: `
+            SELECT item."_id" as "ItemId"
+            , item."name" as "ItemName", item."price", item."description", item."image", item."fk_group_id", item."info",
+            "itemgroup"."name" as "itemGroupName"
+            FROM item
+            INNER JOIN "public"."itemgroup" ON "public"."item"."fk_group_id" = "public"."itemgroup"."_id"
+            ` };
+
+        output = await poolClient.query(query);
+        //console.log("we got output, here it is:", output);
+        return output;
+
+        // await poolClient.query("COMMIT");
+    } catch (err) {
+        console.error("GetPostItems error :", err);
+    } finally {
+        await poolClient.release();
+        await poolClient.end();
+    }
+
+    return output;
+}
+
+// we get all the merge data, which we need for the order documents on mongodb
+export async function GetOrders() {
+
+
+    const pool = new Pool({
+        user: postgres_user,
+        host: postgres_host,
+        database: postgres_db,
+        password: postgres_pass,
+        port: postgres_port
+    })
+
+
+    const poolClient = await pool.connect();
+
+    let output;
+
+    try {
+        const query = `
+        SELECT
+        "public"."order"."_id" as "order_id",
+        "public"."order"."fk_user_id",
+        
+        "public"."user"."_id" as "user_id",
+        "public"."user"."username",
+        "public"."user"."password",
+        "public"."user"."email",
+        "public"."user"."phone",
+        "public"."user"."address",
+        
+        "public"."orderitem"."count",
+        "public"."orderitem"."fk_order_id",
+        "public"."orderitem"."price" as "orderItem_price",
+        "public"."orderitem"."fk_item_id",
+        "public"."orderitem"."_id" as "orderItem_id",
+        
+        "public"."item"."_id" as "item_id",
+        "public"."item"."name" as "item_name",
+        "public"."item"."price" as "item_price",
+        "public"."item"."info",
+        "public"."item"."description",
+        "public"."item"."image",
+        "public"."item"."fk_group_id",
+        
+        "public"."itemgroup"."_id" as "itemGroup_id",
+        "public"."itemgroup"."name" as "itemGroup_name"
+        FROM
+        "public"."item"
+        INNER JOIN "public"."orderitem" ON "public"."orderitem"."fk_item_id" = "public"."item"."_id"
+        INNER JOIN "public"."order" ON "public"."orderitem"."fk_order_id" = "public"."order"."_id"
+        INNER JOIN "public"."user" ON "public"."order"."fk_user_id" = "public"."user"."_id"
+        INNER JOIN "public"."itemgroup" ON "public"."item"."fk_group_id" = "public"."itemgroup"."_id"
+        
+        ORDER BY "public"."order"."_id"
+        `;
+
+        output = await poolClient.query(query);
+
+        // await poolClient.query("COMMIT");
+    } catch (err) {
+        console.error("GetPostItems error :", err);
+    } finally {
+        await poolClient.release();
+        await poolClient.end();
+    }
+
+    return output;
+}
+
+export async function GetStorages() {
+    return "yes"
+}
+
+export async function GetUsers() {
+    const pool = new Pool({
+        user: postgres_user,
+        host: postgres_host,
+        database: postgres_db,
+        password: postgres_pass,
+        port: postgres_port
+    })
+
+
+    const poolClient = await pool.connect();
+
+    let output;
+
+    try {
+        const query = `
+        SELECT
+        "_id",
+        "username",
+        "password",
+        "email",
+        "phone",
+        "address"
+        FROM
+        "public"."user"
+        `;
+
+        output = await poolClient.query(query);
+
+        // await poolClient.query("COMMIT");
+    } catch (err) {
+        console.error("GetPostItems error :", err);
+    } finally {
+        await poolClient.release();
+        await poolClient.end();
+    }
+
+    return output;
+}
---
+++ seeder/src/app.ts
@@ -0,0 +1,120 @@
+
+import { setDbAndFirstUser, AddData } from "./MongodbSeeder";
+import { setPostgresDb, AddPostgresData, setAllTables, DropTable, GetPostItems, GetOrders, GetUsers } from "./PostgresSeeder";
+
+// mongo
+import { Item } from "./entities/mongo/Item";
+import { Storage } from "./entities/mongo/Storage";
+import { ItemGroup } from "./entities/mongo/ItemGroup";
+import { User } from "./entities/mongo/User";
+import { OrderDocument } from "./entities/mongo/OrderDocument";
+
+// postgres
+import { Item_post } from "./entities/postgres/Item";
+import { ItemGroup_post } from "./entities/postgres/ItemGroup";
+import { Order_post } from "./entities/postgres/Order";
+import { OrderItem_post } from "./entities/postgres/OrderItem";
+import { User_post } from "./entities/postgres/User";
+import { Storage_post } from "./entities/postgres/Storage";
+
+// data
+import ItemData from "./data/Items.json";
+import ItemGroupData from "./data/ItemGroup.json";
+import OrderData from "./data/Orders.json";
+import OrderItemData from "./data/OrderItems.json";
+import UserData from "./data/Users.json";
+
+const itemDescription = {
+    name: "item",
+    keys: ["name","price","info","description","image","fk_group_id"]
+};
+const ItemGroupDescription = {
+    name: "itemGroup",
+    keys: ["name"]
+};
+const OrderDescription = {
+    name: `"order"`,
+    keys: ["fk_user_id"]
+};
+const OrderItemDescription = {
+    name: "orderItem",
+    keys: ["fk_item_id","fk_order_id","count","price"]
+};
+const userDescription = {
+    name: `"user"`,
+    keys: ["username","password","email","phone","address"]
+};
+
+async function endAll() {
+    process.exit(0);
+}
+
+async function PostgresSeeding()
+{
+    // postgres
+    await setPostgresDb();
+
+    // drop tables
+    // await DropTable("orderitem");
+    // await DropTable(`"order"`);
+    // await DropTable("item");
+    // await DropTable(`"user"`);
+    // await DropTable("itemgroup");
+
+    await setAllTables();
+
+    // no fk
+    await AddPostgresData<ItemGroup_post>(ItemGroupDescription.name, ItemGroupDescription.keys, ItemGroupData as ItemGroup_post[]);
+    await AddPostgresData<User_post>(userDescription.name, userDescription.keys, UserData as User_post[])
+
+    // have fk
+    await AddPostgresData<Item_post>(itemDescription.name, itemDescription.keys, ItemData as Item_post[])
+    await AddPostgresData<Order_post>(OrderDescription.name, OrderDescription.keys,  OrderData as Order_post[]);
+    await AddPostgresData<OrderItem_post>(OrderItemDescription.name, OrderItemDescription.keys, OrderItemData as OrderItem_post[]);
+}
+
+async function main() {
+
+    console.log("start of the seeder 'main' function");
+
+    
+
+    // get the denormalized, query-shaped documents of the data we added to the postgres
+    
+
+    // mongo
+    // await setDbAndFirstUser();
+    // await AddData<Item>(ItemData, "item");
+    // await AddData<Order>(OrderData, "order");
+    // await AddData<OrderItem>(OrderItemData, "orderitem");
+    // await AddData<ItemGroup>(ItemGroupData, "itemgroup");
+    // await AddData<User>(UserData, "user");
+
+    //let allTheNewItemJoinedDataFromPostgres = await GetPostItems();
+    //console.log("test1 values - get postItems:", allTheNewItemJoinedDataFromPostgres);
+
+
+
+    //let test2 = await GetOrders();
+    //console.log("test2 values - get GetOrders:", test2);    
+    // let test3 = await GetUsers();
+    // console.log("test3 values - get GetUsers:", test3);
+
+    await endAll();
+
+    return "done";
+}
+
+main().then(message => {
+    console.log("message:", message);
+    if(message == "done")
+    {
+        console.log("plz stop this program");
+        endAll();
+        
+    }
+}).catch(err => {
+    console.log("seeder - error", err);
+    //process.exit(0);
+    //process.exit(1);
+});
